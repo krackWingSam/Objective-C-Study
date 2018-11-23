b54d1f3423491f9e8591b02cbd11d3d9c90f709f
krackWingSam <sangwoo21@askstory.com>
2018-11-23 10:12:18 +0900

    Sample 2

---
+++ Sample02/Sample02/SecondViewController.h
@@ -0,0 +1,13 @@
+//
+//  SecondViewController.h
+//  Sample02
+//
+//  Created by askstory on 2018. 11. 22..
+//  Copyright © 2018년 Askstory. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+@end
